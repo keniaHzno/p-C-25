a698e1a74d2f1f95984e4b6a3721b7ea16020f41
keniaHzno <88795664+keniaHzno@users.noreply.github.com>
2021-09-29 21:21:21 -0500

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,31 @@
+class Paper{
+    constructor(x,y,r){
+        var options ={
+            'isStatic':false,
+            'restitution':0.3,
+            'friction':0,
+            'density':1.2
+        }
+        this.x=x;
+		this.y=y;
+		this.r=r;
+        this.body=Bodies.circle(this.x,this.y,(this.r-20)/2 , options);
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+        //applayForce(){}
+          
+        
+    }
+    display(){
+        var paperpos=this.body.position;
+        push();
+        translate(paperpos.x, paperpos.y);
+        //Mode(CENTER);
+        //fill("purple");
+        ellipse(300,0,40);
+        pop();
+
+
+    }
+
+}
